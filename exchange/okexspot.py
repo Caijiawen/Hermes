45673fa1238dfe93907be48889d8377d4bbff013
蔡嘉文 <caijiawen@caijiawendeMacBook-Pro.local>
2019-02-14 23:22:33 +0800

    add newTick and integrate trades

--- exchange/okexspot.py
+++ exchange/okexspot.py
@@ -1,12 +1,14 @@
 from .constants import *
+from .exchange_datastructure import *
+
 import h5py
 import numpy as np
 import datetime
-from .exchange_datastructure import *
 from collections import OrderedDict
+import time
 
 class OkexSpot(object):
-    def __init__(self,symbol,account,fee):
+    def __init__(self,symbol,account,fee,initTrades=False):
         # super.__init__()
         self.time = None
 
@@ -29,66 +31,149 @@ class OkexSpot(object):
         self.limitOrderDict = OrderedDict()         # 限价单字典
         self.workingLimitOrderDict = OrderedDict()  # 活动限价单字典，用于进行撮合用
         
+
+        self.initTrades = initTrades
         self.tradeCount = 0             # 成交编号
-        self.tradeDict = OrderedDict()  # 成交字典
+        self.workingTradeDict = OrderedDict()  # 成交字典
 
     
     #----------------------------------------------------------------------
     
-    def initDataHandler(self, startTime):
+    def initDataHandler(self, startTime ):
         self.startTime = datetime.datetime.strptime(startTime, '%Y-%m-%d')
         self.time = self.startTime
         self.date = self.time.date()
+        print(self.date)
         self.initDepthData( str(self.date) )
+        if self.initTrades:
+            self.initTradesData( str(self.date) )
 
     def checkDataHandler(self):
         if self.time.date() != self.date:
             self.date = self.time.date()
             self.initDepthData( str(self.date) )
+            if self.initTrades:
+                self.initTradesData( str(self.date) )
+
 
     def checkExitHandler(self, endTime):
+
         if self.time.date() > datetime.datetime.strptime(endTime, '%Y-%m-%d').date():
             return True
         return False
         
     def initDepthData(self, date):
-        with h5py.File(f'/data/{self.symbol}/depth/{date}_depth.h5', 'r') as hf:
+        with h5py.File(f'/raid_disk/Caijiawen/data_saver/data/{self.symbol}/depth/{date}_depth.h5', 'r') as hf:
             self.depthData = hf['data'][:]
         self.depthPointer = 0
         self.depthRow = self.depthData.shape[0]
     
     def initTradesData(self, date):
-        with h5py.File(f'/data/{self.symbol}/trade/{date}_trade.h5', 'r') as hf:
+        with h5py.File(f'/raid_disk/Caijiawen/data_saver/data/{self.symbol}/trade/{date}_trade.h5', 'r') as hf:
             self.tradesData = hf['data'][:]
         self.tradesPointer = 0
         self.tradesRow = self.tradesData.shape[0]
+        self.filterLastdayTrade()
+        print(self.tradesPointer)
     #----------------------------------------------------------------------
 
+
+    def filterLastdayTrade(self):
+        while True:
+            if int(self.tradesData[self.tradesPointer,4]) > int(self.tradesData[self.tradesPointer+1,4]):
+                self.tradesPointer += 1
+            else:
+                break
+
+
     
+    def getTime(self):
+        return self.time
+
     def getAccount(self):
         return self.account
     
     def getDepth(self):
-        # print(self.depthData[0,:][400])
-        # print(self.depthData[0,:][400])
-        # print(self.depthPointer)
         self.thisDepth = DepthData(self.depthData[self.depthPointer, :])
-        # print(depth.best_bid)
-        # print(depth.best_ask)
         return self.thisDepth
     
     def getOrders(self):
-        return self.workingLimitOrderDict.keys()
+        return self.workingLimitOrderDict
     
     def getTrades(self):
-        pass
+        return self.workingTradeDict
     
     #----------------------------------------------------------------------
     
     def newTick(self):
         """新的Tick(一轮) not implemented yet"""
-        pass
-    
+        self.newTick_updateDepth()
+        self.newTick_updateTrades()
+
+
+    def newTick_updateDepth(self):
+        self.depthPointer += 1
+
+        if self.depthPointer <= self.depthRow - 1:
+            machineTime = self.depthData[self.depthPointer, 1]
+            machineTime = datetime.datetime.fromtimestamp(machineTime)
+            self.time = machineTime
+        else:
+            new_date = self.date + datetime.timedelta(days=1)
+            # print(new_date)
+            self.time = datetime.datetime(new_date.year, new_date.month, new_date.day)
+            self.checkDataHandler()
+
+    def newTick_updateTrades(self):
+
+        self.workingTradeDict = OrderedDict()
+
+        if self.tradesPointer > self.tradesRow - 1:
+            return
+
+        machineTime = self.tradesData[self.tradesPointer , 0]
+        machineTime = datetime.datetime.fromtimestamp(machineTime)
+
+        while machineTime <= self.time and self.tradesPointer <= self.tradesRow - 1:
+            self.addTrades()
+            self.tradesPointer += 1
+            if self.tradesPointer <= self.tradesRow - 1:
+                machineTime = self.tradesData[self.tradesPointer, 0]
+                machineTime = datetime.datetime.fromtimestamp(machineTime)
+
+    def addTrades(self):
+        """把trades加入字典"""
+
+        self.tradeCount += 1
+        tradeID = str(self.tradeCount)
+
+        trade = TradeData()
+        trade.symbol = self.symbol
+        trade.machineTime = self.tradesData[self.tradesPointer, 0]
+        trade.direction = self.tradesData[self.tradesPointer , 5]
+        trade.price = self.tradesData[self.tradesPointer , 2]
+        trade.volume = self.tradesData[self.tradesPointer , 3]
+        trade.exchangeID = self.tradesData[self.tradesPointer , 1]
+
+        h,m,s = self.number_to_hour_minute_second(self.tradesData[self.tradesPointer, 4])
+        trade.exchangeTime = time.mktime(datetime.datetime(self.date.year , self.date.month , self.date.day , h , m , s).timetuple())
+
+        # 保存到限价单字典中
+        self.workingTradeDict[tradeID] = trade
+
+        return [tradeID]
+
+    def number_to_hour_minute_second(self, num):
+        num = str(int(num))
+        str_num = (6 - len(num)) * '0' + num
+        h = int(str_num[0:2])
+        m = int(str_num[2:4])
+        s = int(str_num[4:6])
+        return h, m, s
+
+
+
+
     def newTime(self , strategy_round_time):
         """新的Time(一轮）"""
         # update time
@@ -162,21 +247,21 @@ class OkexSpot(object):
                 if order.price > self.getDepth().best_ask:
                     # buy dealed
                     order.status = STATUS_ALLTRADED
-                    print("bid order traded.  price:" + str(order.price) + " amount: " + str(order.volume) )
+                    # print("bid order traded.  price:" + str(order.price) + " amount: " + str(order.volume) )
                     self.account[self.coin_a] += order.volume
                     self.account[self.coin_b] -= order.volume * order.price
-                    print("new account " + str(self.account))
-                    print("balance: " + str(self.account[self.coin_a]*self.getDepth().best_bid+self.account[self.coin_b]))
+                    # print("new account " + str(self.account))
+                    # print("balance: " + str(self.account[self.coin_a]*self.getDepth().best_bid+self.account[self.coin_b]))
                     del self.workingLimitOrderDict[o]
             if order.direction == DIRECTION_SHORT:
                 if order.price < self.getDepth().best_bid:
                     # sell dealed
                     order.status = STATUS_ALLTRADED
-                    print("ask order traded.  price:" + str(order.price) + " amount: " + str(order.volume) )
+                    # print("ask order traded.  price:" + str(order.price) + " amount: " + str(order.volume) )
                     self.account[self.coin_a] -= order.volume
                     self.account[self.coin_b] += order.volume * order.price
-                    print("new account " + str(self.account))
-                    print("balance: " + str(self.account[self.coin_a] * self.getDepth().best_bid + self.account[self.coin_b]))
+                    # print("new account " + str(self.account))
+                    # print("balance: " + str(self.account[self.coin_a] * self.getDepth().best_bid + self.account[self.coin_b]))
 
                     del self.workingLimitOrderDict[o]
     
